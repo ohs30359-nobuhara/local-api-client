bbf3cedec14ec2c2dd75c98a7a52f1f8f07b3018
ynobuhar <ynobuhar@yahoo-corp.jp>
2020-08-30 17:15:21 +0900

    js fileの削除

--- src/server/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const application_1 = require("./application");
-const projectRoute_1 = require("./routing/projectRoute");
-const app = new application_1.Application();
-app.use('/resources/project', projectRoute_1.projectRouter);
-app.run();
